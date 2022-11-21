a625a970e8601a84372b154ea14d99839160506b
INGllOT <wojcio4126272@gmail.com>
2022-11-21 14:06:10 +0100

    First commit, with all files

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
@@ -0,0 +1,10 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CurrencyExchangeRepository 
+	extends JpaRepository<CurrencyExchange, Long> {
+	CurrencyExchange findByFromAndTo(String from, String to);
+
+	CurrencyExchange findAllById(int id);
+}
